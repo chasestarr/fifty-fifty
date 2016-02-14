4c12eca6937c6488b66265a7d0e789815ed211cb
Chase Starr <chasestarr@gmail.com>
2016-02-13 21:30:10 -0500

    yelp response writes out to json file for easier testing

--- yelpRequest.js
+++ yelpRequest.js
@@ -1,15 +1,22 @@
 var yelpConfig = require('./configs/yelpConfig.js');
 var Yelp = require('yelp');
 var yelp = new Yelp(yelpConfig);
+var fs = require('fs');
 
 var geoObj = {};
 
 yelp.search({ term: 'coffee', location: '33617'}, function(e,res){
+    // console.log(res);
    if(e) return console.log(res);
    var center = res.region.center;
    var out = res.businesses.map(function(element){
        var coordObj = element.location.coordinate;
-       var descript = "Address: " + element.location.address[0] + "<br>Rating: " + element.rating;
+       var rating = Math.floor(element.rating);
+       var stars = "";
+       for(var i = 0; i < rating; i++){
+           stars = stars + "&#9733;";
+       }
+       var descript = "Address: " + element.location.address[0] + "<br>Rating: " + stars;
        var obj = {
            type: "Feature",
            geometry:{
@@ -21,7 +28,8 @@ yelp.search({ term: 'coffee', location: '33617'}, function(e,res){
             description: descript,
             "marker-color": "#fc4353",
             "marker-size": "small"      
-           }
+           },
+           id:element.id
        };
        return obj;
    });
@@ -29,6 +37,8 @@ yelp.search({ term: 'coffee', location: '33617'}, function(e,res){
        center: center,
        geoJson: out
    };
-   console.log(JSON.stringify(geoObj, null, 2));
+   fs.writeFile("results.json", JSON.stringify(geoObj, null, 2), function(e){
+       if(e) return console.log(e);
+       console.log("the file was saved");
+   })
 });
-
