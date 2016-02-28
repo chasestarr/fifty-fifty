dc466342b9469bed494955b9b07fb946967d2c53
Chase Starr <chasestarr@gmail.com>
2016-02-28 02:02:28 -0500

    fixed

---
+++ config/mongoose/seed.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var schema = require('../schema/databaseSchema');
+
+module.exports = function(db){
+    // mongoose command to drop your db
+     db.db.dropDatabase();
+
+    // seed restaurant
+    for (var item of restaurantSeeds()) {
+        var seedrestaurantitem = schema.restaurant(item);
+        seedrestaurantitem.save(function (err, editedDoc) {
+            if (err) return console.error(err);
+        });
+    }
+};
+
+function restaurantSeeds(){
+    return[{ restaurantId: 'hello world', host: true },{ restaurantId : "fifty-fifty-coffee-and-tea-san-francisco", host : true },{ restaurantId : "coffeeshop-san-francisco", host : true }]
+}
