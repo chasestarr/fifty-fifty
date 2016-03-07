df91ad4b9fdbecafb341329a1f144cb0597deaf1
Chase Starr <chasestarr@gmail.com>
2016-03-06 22:09:17 -0500

    edit procfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dbConfig = require('./config/configFiles/dbConfig.js');
 
 //vars set within the gulpfile. 
 let nodePort = process.env.PORT || 3000;
-let dbConn = 'mongodb://heroku_gqh3ghk1:gqh3ghk1@ds023478.mlab.com:23478/heroku_gqh3ghk1';
+let dbConn = process.env.DBCONN || 'mongodb://'+ dbConfig.username + ':' + dbConfig.password + '@ds023478.mlab.com:23478/heroku_gqh3ghk1';
 
 const express = require('express');
 const cons = require('consolidate');
